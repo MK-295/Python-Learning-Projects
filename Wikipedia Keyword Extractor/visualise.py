983e62aaed0ee8d1eaed0c74001e0043099ee047
MK-295 <75392419+MK-295@users.noreply.github.com>
2020-12-19 20:40:14 +0000

    Update visualise.py

--- Wikipedia Keyword Extractor/visualise.py
+++ Wikipedia Keyword Extractor/visualise.py
@@ -12,17 +12,18 @@ the word cloud takes in a list of words
 def bar_chart(input: dict, ax):
     keys = input.keys()
     values = input.values()
-    ax.set_xticklabels(list(keys), Rotation=90)
-    ax.bar(keys, values, width=0.3, color='orange') 
-    ax.set_title('Bar Chart - Keyword Frequency')
+    ax.set_xticklabels(list(keys), Rotation=90,fontsize=7)
+    ax.bar(keys, values, width=0.3, color='lightseagreen') 
+    ax.set_title('Bar Chart - Keyword Frequency',fontsize=8)
     return ax
    
 def visualise_piechart(input: dict,ax):
     words = list(input.keys())
     values = list(input.values())
-    ax.pie(values, labels=words)
+    color=['lightseagreen','mediumslateblue','indigo','yellowgreen','gold','lightcoral','olivedrab','lightpink','plum']
+    ax.pie(values, labels=words,colors=color,textprops={'fontsize': 7})
     ax.axis('equal') 
-    ax.set_title('Pie Chart - Keyword Frequency')
+    ax.set_title('Pie Chart - Keyword Frequency',fontsize=8)
     return ax
 
 
@@ -33,6 +34,6 @@ def word_cloud_2(input2: list,ax):
     wordcloud = WordCloud().generate(string)                                              
     wordcloud = WordCloud(background_color="white",max_words=len(string),max_font_size=30, relative_scaling=.5).generate(string)
     ax.imshow(wordcloud)
-    ax.set_title('Wikipedia Data Word Cloud')
+    ax.set_title('Wikipedia Data Word Cloud',fontsize=8)
     ax.axis("off")
     return ax
