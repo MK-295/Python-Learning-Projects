263f6decc1fdc9bf889a4b2779689577f1933b5e
MK-295 <75392419+MK-295@users.noreply.github.com>
2020-12-22 20:49:52 +0000

    Update server.py

--- Flask API Login App/server.py
+++ Flask API Login App/server.py
@@ -1,1 +1,27 @@
-
+def store_user_info(username, password): 
+    '''
+    This function takes in a username and password as a string, checks if it is in the database, if it is then a new username is required, 
+    otherwise the users details will be added to the database
+    '''
+    username=str(username)
+    password=str(password)
+    database={'sarah': 'dolphins','tom':'puppies'}
+    if username in database:
+        print ('Username already exists, please try again')
+    else:
+        database.update({username : password})
+        print ('Details updated')
+    return database
+  
+def check_user_info(username, password):
+    '''
+    Checks if the username and password a user has entered matches with the details in the database
+    '''
+    
+    database={'sarah': 'dolphins','tom':'puppies'}
+    if (username, password) in database.items():
+        True
+    else: 
+        False
+    return 
+    
